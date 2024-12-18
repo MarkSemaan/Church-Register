bda6764c6aca36c5456be1ff7b9116e9720c0adb
MarkSemaan <mark.h.semaan@gmail.com>
2024-12-18 13:38:58 +0200

    Cleaned some code

--- vue.config.js
+++ vue.config.js
@@ -2,20 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   pluginOptions: {
-    electronBuilder: {
-      builderOptions: {
-
-        mac:{
-          "target": [
-            {
-              "target": "dmg",
-              "arch": [
-                "universal"
-              ]
-            }
-          ]
-        }
-      }
-    }
   }
 })
